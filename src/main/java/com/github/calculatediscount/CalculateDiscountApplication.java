ccedcb18f3c1aacd2bf05214c567964cb8ea49e2
Guilherme Costa Sales <216333@contact.grupocsu.local>
2024-05-19 23:23:45 -0300

    init: create initial project structure

---
+++ src/main/java/com/github/calculatediscount/CalculateDiscountApplication.java
@@ -0,0 +1,13 @@
+package com.github.calculatediscount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculateDiscountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculateDiscountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/calculatediscount/CalculateDiscountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.calculatediscount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculateDiscountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
